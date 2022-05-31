36b84b0ddb11ef48e500475b7e4659b96f6f7e43
Nellykhris <ifeomaomeonu@gmail.com>
2022-05-31 05:42:25 +0100

    Binary uint

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -3,8 +3,8 @@
 /**
  *  * binary_to_uint - converts a binary number to unsigned int
  *   * @b: string containing the binary number
- *
- *Return: the converted number
+ *    *
+ *     * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
